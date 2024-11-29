76ba90357dde5ab5a37ee0790845151b861ec118
Rajendra Pandey <rajendrapandey199971@gmail.com>
2024-11-29 15:11:28 +0545

    learned for loop iterate

---
+++ Chapter-7/05_for_loop_iterate.py
@@ -0,0 +1,18 @@
+# For loops with list
+list = ["Raja", "Milan", 25, 22.5, True, "Rohan"]
+
+for items in list:
+    print(items)
+
+# For loops for tuple
+tuple = (1, 5, 7, 8, 80, "Raja")
+
+for items in tuple:
+    print(items)
+
+
+# For loops for string
+s = "Rajendra"
+
+for alp in s:
+    print(alp)
