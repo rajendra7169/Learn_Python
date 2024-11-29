2e8704dcd459f01a441c6298c8001650d00d6838
Rajendra Pandey <rajendrapandey199971@gmail.com>
2024-11-29 15:12:09 +0545

    learned loop with break

---
+++ Chapter-7/07_for_loop_with_break.py
@@ -0,0 +1,13 @@
+# Description: This script demonstrates the use of break statement in for loop
+for item in range(100):
+    if item == 60:
+        break  # This will break the loop when item is 60
+    print(item)
+
+# Continue statement:
+for i in range(100):
+    if i == 60:
+        continue  # This will skip the rest of the code and continue with the next iteration
+    print(i)
+
+# This program will print all the numbers from 0 to 99 except 60
