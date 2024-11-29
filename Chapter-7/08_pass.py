011e4497408cc1087ff2a49ef9dc1cf0ce11ecd6
Rajendra Pandey <rajendrapandey199971@gmail.com>
2024-11-29 15:12:30 +0545

    learned pass.py

---
+++ Chapter-7/08_pass.py
@@ -0,0 +1,9 @@
+# Description: This program demonstrates the use of pass statement in Python
+for items in range(100):
+    pass  # This will do nothing
+
+i = 0
+
+while i < 10:
+    print(i)
+    i += 1
