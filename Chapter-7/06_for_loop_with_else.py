406b8754a7ca3cf389f4fe1203a619d792667e81
Rajendra Pandey <rajendrapandey199971@gmail.com>
2024-11-29 15:11:47 +0545

    learned loop with else

---
+++ Chapter-7/06_for_loop_with_else.py
@@ -0,0 +1,11 @@
+list = [1, 7, 9]
+
+for item in list:
+    print(item)
+
+else:
+    print("No item left in the list")
+
+print("End of the loop")
+
+# This program will print all the items in the list and then print "No item left in the list" and "End of the loop" at the end.
