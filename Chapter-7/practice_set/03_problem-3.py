953bc4fe59cd80677d3fed65285a75641f18a1d4
Rajendra Pandey <rajendrapandey199971@gmail.com>
2024-11-29 15:15:39 +0545

    problem 3 Attempt problem 1 using while loop

---
+++ Chapter-7/practice_set/03_problem-3.py
@@ -0,0 +1,13 @@
+# Q-3 Attempt problem 1 using while loop.
+
+num = int(input("Enter a number: "))
+i = 1
+
+while i < 11:
+    print(f"{num}*{i} = {num*i}")
+    i += 1
+
+# num = int(input("Enter a number: "))
+
+# for i in range(1, 11):
+#     print(f"{num} * {i} = {num*i}")
