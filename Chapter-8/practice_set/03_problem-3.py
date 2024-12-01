b3a59371e28f2df14f28568db12810e0c0192081
Rajendra Pandey <rajendrapandey199971@gmail.com>
2024-12-01 15:57:23 +0545

    prevent a python print() function to print a new line at the end

---
+++ Chapter-8/practice_set/03_problem-3.py
@@ -0,0 +1,10 @@
+# Q-3 How do you prevent a python print() function to print a new line at the end.
+
+# Ans: We can use (end="")  paramater to prevent print() function to print a new line at the end.
+# Example
+
+print("Hi, Raja ", end="")
+print("How are you")
+
+# Output: Hi, Raja How are you
+# Explanation: In the above example, we have used end="" parameter to prevent print() function to print a new line at the end.
