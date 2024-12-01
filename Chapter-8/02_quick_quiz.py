772fd6e52179b7730cc11e53f43aa200315ad84c
Rajendra Pandey <rajendrapandey199971@gmail.com>
2024-12-01 15:53:53 +0545

    quick quiz of function

---
+++ Chapter-8/02_quick_quiz.py
@@ -0,0 +1,6 @@
+def goodDay():  # Camelcase Notation for function name (goodDay) another is snake_case_notation (good_day)
+    name = input("Enter your namne: ")
+    print("Have Good Day", name)
+
+
+goodDay()
