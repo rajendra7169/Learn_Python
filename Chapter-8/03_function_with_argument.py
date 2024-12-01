7fc34a6002c3c2c255fbead00d6e722b6452c7ce
Rajendra Pandey <rajendrapandey199971@gmail.com>
2024-12-01 15:54:08 +0545

    learned function with agrument

---
+++ Chapter-8/03_function_with_argument.py
@@ -0,0 +1,17 @@
+# def good_day(name, ending):
+#     print("Good day, " + name, ending)
+#     # print(ending)
+
+
+# good_day("Raja", "Thank You.")
+# good_day("Milan", "Thanks.")
+
+
+def good_day(name, ending):
+    print("Good day, " + name, ending)
+    # print(ending)
+    return "Bye"
+
+
+a = good_day("Raja", "Thank You.")
+print(a)
